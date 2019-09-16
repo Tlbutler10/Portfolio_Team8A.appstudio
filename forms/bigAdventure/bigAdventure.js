19eaae743a49bacbe44dacab6b8b72854b3742b8
Tanner <tannerbutler15@gmail.com>
2019-09-15 22:04:23 -0500

    update

--- forms/bigAdventure/bigAdventure.js
+++ forms/bigAdventure/bigAdventure.js
@@ -4,20 +4,181 @@ alert("You must visit each of the six rooms at least once to win the game")
 alert("The program will not stop until you win")
 alert("This is how you move: You will be given a choice to go forward, left, or right?")
 prompt('Are you ready')
-let nameOFGame = "The Big Adventure"
 let moveArray = []
 let kitchen = ['forward']
-let masterBedroom = ['forward', 'forward']
+let masterBedroom = ['forward','forward']
 let bathroom = ['forward','forward','forward']
-let livingRoom = ['forward','forward','forward', 'right'] 
-let gameRoom = ['forward','forward','forward', 'right', 'left']
-let manCave = ['forward','forward','forward', 'right', 'left','right']
+let livingRoom = ['forward','forward','forward','right'] 
+let gameRoom = ['forward','forward','forward','right','left']
+let manCave = ['forward','forward','forward','right','left','right']
 let move1 = prompt('forward, left, or right')
 moveArray.push(move1)
 console.log(moveArray);
 
+if (moveArray = ['forward']){
+alert('Welcome to the Kitchen');
+let move2 = prompt('forward, left, or right');
+moveArray.push(move2);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward']){
+  alert('Welcome to the Master Bedroom');
+let move3 = prompt('forward, left, or right');
+moveArray.push(move3);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward']){
+  alert('Welcome to the Bathroom');
+let move4 = prompt('forward, left, or right');
+moveArray.push(move4);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right']){
+ alert('Welcome to the Living Room');
+let move5 = prompt('forward, left, or right');
+moveArray.push(move5);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right','left']){
+ alert('Welcome to the Game Room');
+let move6 = prompt('forward, left, or right');
+moveArray.push(move6);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right','left','right']){
+  alert('Welcome to the Man Cave');
+  let nameOfGame = "The Big Adventure";
+alert(`Congratulations, you have won ${nameOfGame}`);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+}
+if (moveArray = ['forward']){
+alert('Welcome to the Kitchen');
+let move2 = prompt('forward, left, or right');
+moveArray.push(move2);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward']){
+  alert('Welcome to the Master Bedroom');
+let move3 = prompt('forward, left, or right');
+moveArray.push(move3);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward']){
+  alert('Welcome to the Bathroom');
+let move4 = prompt('forward, left, or right');
+moveArray.push(move4);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right']){
+ alert('Welcome to the Living Room');
+let move5 = prompt('forward, left, or right');
+moveArray.push(move5);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right','left']){
+ alert('Welcome to the Game Room');
+let move6 = prompt('forward, left, or right');
+moveArray.push(move6);
+console.log(moveArray);
+} else if (moveArray = ['forward','forward','forward','right','left','right']){
+  alert('Welcome to the Man Cave');
+  let nameOfGame = "The Big Adventure";
+alert(`Congratulations, you have won ${nameOfGame}`);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+}
+
+
+
+/*
+
+if (moveArray = ['forward']){
+alert('Welcome to the Kitchen');
+let move2 = prompt('forward, left, or right');
+moveArray.push(move2);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+let moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+  
+if (moveArray = ['forward','forward']){
+  alert('Welcome to the Master Bedroom');
+let move3 = prompt('forward, left, or right');
+moveArray.push(move3);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+  
+if (moveArray = ['forward','forward','forward']){
+  alert('Welcome to the Bathroom');
+let move4 = prompt('forward, left, or right');
+moveArray.push(move4);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+
+if (moveArray = ['forward','forward','forward','right']){
+ alert('Welcome to the Living Room');
+let move5 = prompt('forward, left, or right');
+moveArray.push(move5);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+  
+if (moveArray = ['forward','forward','forward','right','left']){
+ alert('Welcome to the Game Room');
+let move6 = prompt('forward, left, or right');
+moveArray.push(move6);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+  
+if (moveArray = ['forward','forward','forward','right','left','right']){
+  alert('Welcome to the Man Cave');
+  let nameOfGame = "The Big Adventure";
+alert(`Congratulations, you have won ${nameOfGame}`);
+console.log(moveArray);
+} else {
+  moveArray.pop();
+console.log(moveArray);
+moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
+moveArray.push(moveRandom);
+console.log(moveArray);
+  }
+*/
+
+
+
+/*
+
 switch(moveArray) {
-case kitchen:
+case ('forward'):
 alert('Welcome to the Kitchen');
 let move2 = prompt('forward, left, or right');
 moveArray.push(move2);
@@ -53,35 +214,10 @@ alert(`Congratulations, you have won ${nameOfGame}`);
 console.log(moveArray);
 break;
 default:
-prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
-let moveRandom = prompt('forward, left, or right');
+moveArray.pop();
+console.log(moveArray);
+let moveRandom =prompt('Sorry, you have hit a wall choose a different option, forward, left, or right');
 moveArray.push(moveRandom);
 console.log(moveArray);
 }
-
-/*
-
-let rooms = []
-alert("Welcome to the game, your task is to navigate your way through the house. Once you make it through all six rooms, you win.")
-let ready = prompt("Are you ready?")
-if (ready === "no" || ready === "No" || ready === "NO") {
-  alert("You lose")
-} else if (ready === "yes" || ready === "Yes" || ready === "YES"){
-  alert("You enter the house")
-
-moveArray.push(move1)
-let move2 = prompt('forward, left, or right')
-let move3 = prompt('forward, left, or right')
-let move4 = prompt('forward, left, or right')
-let move5 = prompt('forward, left, or right')
-let move6 = prompt('forward, left, or right')
-
-
-
-
-
-
-
-} else { alert("I take that as a no, you lose.") }
-
 */
